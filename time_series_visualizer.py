8846631e77964af9bf0c7dc1fc0ee2130da911a8
Sam Anderson <samdanderson27@gmail.com>
2024-06-12 17:59:35 +0000

    feat: finished draw_bar_plot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -37,155 +37,50 @@ def draw_line_plot():
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
 
-    # OLD: Uses Period columns
-    
       # Convert date index to month column
-    #print(df.head())
     df_period = df.reset_index()
-    #print(df_period.head())
     df_period['month'] = df_period['date'].apply(pd.Period, args=('M'))
     df_period = df_period.drop(columns=['date'])
-    #print(df_period.head())
 
       # Find average page views per month
     df_month_avg = df_period.groupby('month').mean()
-    #print(df_month_avg.head())
 
       # Add year column
     df_bar = df_month_avg.reset_index()
-    #print(df_bar.head())
-    #df_bar['year'] = df_bar['month'].apply(pd.Period, args=('Y'))
     df_bar['year_trunc'] = df_bar['month'].apply(lambda x: x.year)
-    #print(df_bar.head())
 
-      # Truncate month column (not sure if necessary? TBD)
+      # Truncate month column
     df_bar['month_trunc'] = df_bar['month'].apply(lambda x: x.month)
     print(df_bar.head())
     print(df_bar.info())
-    
-
-    # NEW: Uses DateTime columns
-    '''
-          # Convert date index to month column
-    print(df.head())
-    df_period = df.reset_index()
-    print(df_period.head())
-    #df_period['month'] = df_period['date'].apply(pd.Period, args=('M'))
-    df_period['month'] = df_period['date'].apply(lambda x: pd.Timestamp(year = x.year, month = x.month, day = None))
-    df_period = df_period.drop(columns=['date'])
-    print(df_period.head())
-
-      # Find average page views per month
-    df_month_avg = df_period.groupby('month').mean()
-    print(df_month_avg.head())
-
-      # Add year column
-    df_bar = df_month_avg.reset_index()
-    print(df_bar.head())
-    #df_bar['year'] = df_bar['month'].apply(pd.Period, args=('Y'))
-    df_bar['year'] = df_bar['month'].apply(lambda x: pd.Timestamp(year = x.year, month = None, day = None))
-    print(df_bar.head())
-
-      # Truncate month column (not sure if necessary? TBD)
-    df_bar['month_trunc'] = df_bar['month'].apply(lambda x: pd.Timestamp(month = x.month, year = None, day = None))
-    print(df_bar.head())
-    print(df_bar.info())
-    '''
 
     # Draw bar plot
     fig = plt.figure()
-
-    #Step 1: bar chart for 2016
-    '''
     ax = fig.add_subplot(1,1,1)
-    copy = df_bar.loc[df_bar['year_trunc'] == 2017]
-    x = copy['month_trunc']
-    height = copy['value']
-    ax.bar(x, height)
-    ax.set_xlabel('Years')
-    ax.set_ylabel('Average Page Views')
-    '''
-
-    #Step 2: December by year
-    '''
-    ax = fig.add_subplot(1,1,1)
-    december = 12
-    dec = df_bar.loc[df_bar['month_trunc'] == december]
-    month_names = ['January','February','March','April','May','June','July','August','September','October','November','December']
-    ax.bar(dec['year_trunc'], dec['value'], label=month_names[december-1])
-
-
-    ax.legend()
-    ax.set_xlabel('Years')
-    years = df_bar['year_trunc'].unique()
-    width = 0.25
-    num_years = years.size
-    x = [i + width for i in range(num_years)]
-    print(years)
-    print(num_years)
-    #ax.set_xticks(x, years)
-    ax.set_ylabel('Average Page Views')
-    '''
-
-
-    # Step 3: Plot each month
-    '''
-    ax = fig.add_subplot(1,1,1)
-    month_names = [None,'January','February','March','April','May','June','July','August','September','October','November','December']
-    years = df_bar['year_trunc'].unique()
-    num_years = years.size
-    first_year = years[0]
-    print(first_year)
-    width = 0.1
-    months = range(1,13)
-
-    for month in months:
-        df_month = df_bar.loc[df_bar['month_trunc'] == month]
-        # Determine relative position:
-          # 0 is January of first year
-          # 1 is February of first year
-          # 13 is January of second year
-        x = [(12 * (year - first_year)) + (month - 1) for year in df_month['year_trunc']]
-        ax.bar(x, df_month['value'], label=month_names[month])
     
-
-    # Add legends and such
-    ax.legend()
-    ax.set_xlabel('Years')
-    ax.set_ylabel('Average Page Views')
-    # Determine relative position:
-      # 6 is first year
-      # 18 is second year
-      # 6 + ((year - first_year) * 12)
-    #xticks = [6 + ((year - first_year) * 12) for year in years]
-    xticks = [0,1,2,3]
-    ax.set_xticks(x, years)
-    '''
-
-    # Step 4: Add width
-    ax = fig.add_subplot(1,1,1)
     month_names = [None,'January','February','March','April','May','June','July','August','September','October','November','December']
     years = df_bar['year_trunc'].unique()
-    num_years = years.size
     first_year = years[0]
-    #x = [0,2,4,6] # Base locations: I did the math
     width = 0.1 # Width of each bar
+
+    # Plot by month
     months = range(1,13)
     for month in months:
         df_month = df_bar.loc[df_bar['month_trunc'] == month]
+        # Calculate positions for each month
+        # 0.0: January of first year, 0.1: February of first year... 1.1: December of first year
+        # 2.0: January of second year...
         offset = width * (month-1)
         x = [(2 * (year - first_year)) + offset for year in df_month['year_trunc']]
-        #x_mod = [n + offset for n in x]
         ax.bar(x, df_month['value'], width, label=month_names[month])
 
+    # Set ticks to the middle of each year's bars
     x_ticks = [((year - first_year) * 2) + (width * (len(months) / 2)) for year in years]
-    print(x_ticks)
-    # ((year - first_year) * 2) + (width * len(months))
-    x = [0.6,2.6,4.6,6.6]
     ax.set_xticks(x_ticks, years)
-    ax.legend()
-    ax.set_xlabel('Years')
-    ax.set_ylabel('Average Page Views')
+
+    ax.legend(fontsize='small')
+    ax.set_xlabel('Years', fontsize='small')
+    ax.set_ylabel('Average Page Views', fontsize='small')
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
