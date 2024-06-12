9f9937ce15e478ac7cb3eb71aaf2a15c5f14da10
Sam Anderson <samdanderson27@gmail.com>
2024-06-12 13:24:24 +0000

    feat: added draw_line_plot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,18 +5,27 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("fcc-forum-pageviews.csv", index_col='date', parse_dates=['date'])
+
 
 # Clean data
-df = None
+description = df.describe(percentiles=[0.025, 0.975])
+small_cutoff = description.loc['2.5%'][0]
+big_cutoff = description.loc['97.5%'][0]
+df = df.loc[(df['value'] > small_cutoff) & (df['value'] < big_cutoff)]
 
 
 def draw_line_plot():
     # Draw line plot
+    x = df.index
+    y = df['value']
 
-
-
-
+    fig = plt.figure(figsize=(15, 4.8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(x,y)
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
